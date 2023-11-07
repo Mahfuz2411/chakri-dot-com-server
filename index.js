77ba91754a2149ec8caa4b70018417a92b4c761d
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-11-07 19:03:09 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const jobsCollection = client.db('chakriDB').collection('jobs');
+    const bidsCollection = client.db("chakriDB").collection('bids');
+
+    app.get('/', (req, res) => {
+      res.send('Sweet Home');
+    });
+    
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`)
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
